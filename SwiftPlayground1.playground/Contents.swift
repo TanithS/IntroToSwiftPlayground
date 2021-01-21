d77c73ea53bc1ce4b4c6c65af3954490832703b8
TanithS <77208045+TanithS@users.noreply.github.com>
2021-01-21 12:25:33 -1000

    Assignment #2: Parts 1-5 Complete
    Class 4 - Thursday, January 21, 2021: Completed parts 1-5 for Assignment #2

--- SwiftPlayground1.playground/Contents.swift
+++ SwiftPlayground1.playground/Contents.swift
@@ -3,15 +3,19 @@ import UIKit
 var str = "Hello, playground"
 
 //declare a variable, name it, myFirstVariable and type it as a String
+
 var myFirstVariable: String
+
 //String is a data type that is represented by a series of characters. The string must be surrounded by ""
 
 //assign myFirstVariable to a string
+
 myFirstVariable = "I've created my first string"
 
 //String Concatenation - combining strings together
 
 //declare and assign a second variable all in one line
+
 var mySecondVariable = " and look, I added two strings together!"
 
 //create a third variable to hold string concatenation
@@ -20,12 +24,29 @@ var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
-var newVariable1 = "A haiku for you"
+var newVariable1 = "\nA haiku for you"
 
 var newVariable2 = "\nConcatenating new strings"
 
-var newVariable3 = "\nCan be very fun"
+var newVariable3 = "\nCan be very fun\n"
 
 var myFourthVariable = newVariable1 + newVariable2 + newVariable3
 
 print(myFourthVariable)
+
+
+//Assignment #2: Introduction to True and False - Class 4 Thursday, January 21, 2021
+
+var appState = false
+
+print(appState)
+
+//declared and assigned two variables of type int
+
+var numberOne = 5
+
+var numberTwo = 7
+
+var comparison = numberOne > numberTwo
+
+print(comparison)
